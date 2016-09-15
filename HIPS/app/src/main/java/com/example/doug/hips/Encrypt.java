485b8b491945b9a31153931e87c776feec01f0f4
drew <mortonwd@appstate.edu>
2016-09-15 11:20:16 -0400

    Trying to set up SMS. Eliminated the 'Display Screen' to show encryption version

--- HIPS/app/src/main/java/com/example/doug/hips/Encrypt.java
+++ HIPS/app/src/main/java/com/example/doug/hips/Encrypt.java
@@ -5,27 +5,103 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.PendingIntent;
+import android.telephony.SmsManager;
+import android.util.Log;
+
+
 
 public class Encrypt extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.example.HIPS.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encrypt);
+        //EditText sending_message = (EditText) findViewById(R.id.sending_message);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+                                       public void onClick(View view) {
+                                           sendSMSMessage();
+                                       }
     }
 
-    /** Called when the user clicks the Send button*/
+    /** Called when the user clicks the Encrypt button*/
     public void encryptMessage(View view) {
         Intent intent = new Intent(this, DisplayMessage.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
+        EditText sending_message = (EditText) findViewById(R.id.sending_message);
+        TextView encrypted_MSG = (TextView) findViewById(R.id.encryptedMsg);
         // Stores the data from the input box
-        String message = editText.getText().toString();
+        String message = sending_message.getText().toString();
         //converts the message into a pig latin msg
-        message = convert(message);
+        if (!message.equals("")) {
+            message = convert(message);
+        }
+        else
+        {
+            // Put a pop-up here saying
+            // "Are you sure you want to send a blank message?"
+            // Options "Yes" and "Cancel"
+            message = "Error: Message was blank!";
+        }
+
+        //stores the convertd string into Extra Message
+        // to be used by any other activity
         intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        //startActivity(intent);
+        encrypted_MSG.setText(message);
+        view.invalidate();
+        //setContentView(R.layout.activity_encrypt);
     }
+
+    public void sendMessage(View view) {
+        Intent intent = getIntent();
+        EditText phoneNum = (EditText) findViewById(R.id.phoneNum);
+        String encrypt_sent_MSG = intent.getStringExtra(Encrypt.EXTRA_MESSAGE);
+
+        Log.i("Send SMS", "");
+        String phoneNo = phoneNum.getText().toString();
+        String message = encrypt_sent_MSG;
+
+        //String phoneNo = txtphoneNo.getText().toString();
+        //String message = txtMessage.getText().toString();
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
+        }
+
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+    }
+    private void sendSMS(String phoneNumber, String message)
+    {
+        PendingIntent pi = PendingIntent.getActivity(this, 0,
+                new Intent(this, Encrypt.class), 0);
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, pi, null);
+    }
+    public void decodeMessage(View view) {
+
+    }
+    //@Override
+    /*public void setEncryptedMSG(String message) {
+        //super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        message = intent.getStringExtra(EXTRA_MESSAGE);
+        encrypted_MSG.setText(EXTRA_MESSAGE);
+    }*/
+    /**
+     *
+     * @param x is the string to be converted
+     * @return string converted into pig latin
+     */
     public String convertAlg(String x)
     {
         // Rules:
@@ -66,6 +142,13 @@ public class Encrypt extends AppCompatActivity {
 
         return y;
     }
+
+    /**
+     * Takes a given string and then converts it into a pig latin string
+     * by using the convert alg
+     * @param s given string to be converted
+     * @return converted string
+     */
     public String convert(String s)
     {
         String[] strArr = s.split(" ");
---
+++ HIPS/app/src/main/java/com/example/doug/hips/SMS.java
@@ -0,0 +1,10 @@
+package com.example.doug.hips;
+
+import android.telephony.gsm.SmsManager;
+
+/**
+ * Created by drew on 9/15/16.
+ */
+public class SMS {
+
+}
